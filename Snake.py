9dc5a8a56aecc460268fe0c9643a82033ff365c5
Nox Heidt <nheidt@lr1038.local>
2023-05-15 19:51:33 -0400

    nightly commit

--- Snake.py
+++ Snake.py
@@ -24,7 +24,7 @@ snake_position = [100,50]
 snake_body = [[100,50], [90,50], [80,50], [70,50]]
 
 fruit_position = [random.randrange(1, (window_x//10)) * 10, 
-                    random,randrange(1, (window_y//10)) * 10]
+                    random.randrange(1, (window_y//10)) * 10]
 
 
 fruit_spawn = True
@@ -79,11 +79,11 @@ while True:
 
     if change_to == 'UP' and direction != 'Down':
         direction = 'UP'
-    elif change_to == 'DOWN' and != 'UP':
+    elif change_to == 'DOWN' and direction != 'UP':
         direction = 'DOWN'
-    elif change_to == 'LEFT' != 'RIGHT':
+    elif change_to == 'LEFT' and direction!= 'RIGHT':
         direction = 'LEFT'
-    elif change_to == 'RIGHT' != 'LEFT':
+    elif change_to == 'RIGHT' and direction != 'LEFT':
         direction = 'RIGHT'
 
     
@@ -95,5 +95,49 @@ while True:
         snake_position[0] -= 10
     elif direction == 'RIGHT':
         snake_position[0] += 10
+
+    snake_body.insert(0, list(snake_position))
+    if snake_position[0] == fruit_position[0] and snake_position[1] == fruit_position[1]:
+        score += 10
+        fruit_spawn = False
+    else:
+        snake_body.pop()
+
+    if not fruit_spawn:
+        fruit_position = [random.randrange(1, (window_x//10)) * 10,
+        random.randrange(1, (window_y//10) * 10)]
+
+    fruit_spawn = True
+    game_window.fill(black)
+
+    for pos in snake_body:
+        pygame.draw.rect(game_window, green, pygame. Rect(
+            pos[0], pos[1], 10, 10)
+        )
+
+    pygame.draw.rect(game_window, white, pygame.Rect(
+        fruit_position[0], fruit_position[1], 10, 10)
+    )
+
+    if snake_position[0] < 0 or snake_position[0] > window_x-10:
+        game_over()
+    if snake_position[1] < 0 or snake_position[1] > window_y-10:
+        game_over()
+
+    
+    for block in snake_body[1:]:
+        if snake_position[0] == block[0] and snake_position[1] == block[1]:
+            game_over()
+
+    show_score(1, white, 'times new roman', 20)
+
+    pygame.display.update()
+
+    fps.tick(snake_speed)
+
+
+
+
+    
 
 
